15ca03188e1a9bbc7f80cc25cb868c2ee7ac017f
Sean Milburn <sean.milburn@digital.hmrc.gov.uk>
2015-10-13 23:30:15 +0100

    first commit

---
+++ SalesProApplication/SalesProApplication/urls.py
@@ -0,0 +1,49 @@
+﻿"""
+Definition of urls for SalesProApplication.
+"""
+
+from datetime import datetime
+from django.conf.urls import patterns, url
+from app.forms import BootstrapAuthenticationForm
+
+# Uncomment the next lines to enable the admin:
+# from django.conf.urls import include
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^$', 'app.views.home', name='home'),
+    url(r'^agents$', 'app.views.agents', name='agents'),
+    url(r'^properties$', 'app.views.properties', name='properties'),
+    url(r'^reminders$', 'app.views.reminders', name='reminders'),
+    url(r'^new$', 'app.views.new', name='new'),
+    url(r'^archive$', 'app.views.archive', name='archive'),
+    url(r'^invoice$', 'app.views.invoice', name='invoice'),
+    url(r'^pipeline$', 'app.views.pipeline', name='pipeline'),
+    url(r'^contact', 'app.views.contact', name='contact'),
+    url(r'^login/$',
+        'django.contrib.auth.views.login',
+        {
+            'template_name': 'app/login.html',
+            'authentication_form': BootstrapAuthenticationForm,
+            'extra_context':
+            {
+                'title':'Log in',
+                'year':datetime.now().year,
+            }
+        },
+        name='login'),
+    url(r'^logout$',
+        'django.contrib.auth.views.logout',
+        {
+            'next_page': '/',
+        },
+        name='logout'),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
---
+++ SalesProApplication/app/views.py
@@ -0,0 +1,123 @@
+﻿"""
+Definition of views.
+"""
+
+from django.shortcuts import render
+from django.http import HttpRequest
+from django.template import RequestContext
+from datetime import datetime
+
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/index.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Sales-Pro',
+            'year':datetime.now().year,
+        })
+    )
+def agents(request):
+    """Renders the agents page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/agents.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Agents',
+            'year':datetime.now().year,
+        })
+    )
+def properties(request):
+    """Renders the properties page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/properties.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Properties',
+            'year':datetime.now().year,
+        })
+    )
+def reminders(request):
+    """Renders the reminders page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/reminders.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Reminders',
+            'year':datetime.now().year,
+        })
+    )
+def new(request):
+    """Renders the new page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/new.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'New Sale',
+            'year':datetime.now().year,
+        })
+    )
+def archive(request):
+    """Renders the archive page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/archive.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Archive',
+            'message':'Your application description page.',
+            'year':datetime.now().year,
+        })
+    )
+def invoice(request):
+    """Renders the invoice page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/invoice.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Invoices Due',
+            'message':'Your application description page.',
+            'year':datetime.now().year,
+        })
+    )
+def pipeline(request):
+    """Renders the pipeline page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/pipeline.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Total Pipeline',
+            'message':'Your application description page.',
+            'year':datetime.now().year,
+        })
+    )
+def contact(request):
+    """Renders the contact page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/contact.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Contact',
+            'message':'Your contact page.',
+            'year':datetime.now().year,
+        })
+    )
+
+
