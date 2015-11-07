3e616c6610cac315df16f7dd3b8195932faa0018
Paul <paullehenaff@msn.com>
2015-11-07 23:55:28 +0000

    Added form declarations Updated 'new' view to handle form get and post. Updated moels to separate agent and property. Added agent foreign key to Property model
    Note: Not fully working yet. There's an issue with form validation. Its probly a very basic thing to fix.

--- SalesProApplication/app/forms.py
+++ SalesProApplication/app/forms.py
@@ -6,6 +6,8 @@ from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 from django.utils.translation import ugettext_lazy as _
 
+from .models import Vendor, Purchaser, Agent, Sale
+
 class BootstrapAuthenticationForm(AuthenticationForm):
     """Authentication form which uses boostrap CSS."""
     username = forms.CharField(max_length=254,
@@ -17,4 +19,23 @@ class BootstrapAuthenticationForm(AuthenticationForm):
                                    'class': 'form-control',
                                    'placeholder':'Password'}))
 
-    
+
+class VendorForm(forms.ModelForm):
+    class Meta:
+        model = Vendor
+        exclude = ['id']
+
+class PurchaserForm(forms.ModelForm):
+    class Meta:
+        model = Purchaser
+        exclude = ['id']
+
+class AgentForm(forms.ModelForm):
+    class Meta:
+        model = Agent
+        exclude = ['id']
+
+class SaleForm(forms.ModelForm):
+    class Meta:
+        model = Sale
+        exclude = ['id']
--- SalesProApplication/app/views.py
+++ SalesProApplication/app/views.py
@@ -7,6 +7,8 @@ from django.http import HttpRequest
 from django.template import RequestContext
 from datetime import datetime
 
+from .forms import VendorForm, PurchaserForm, AgentForm, SaleForm
+
 def home(request):
     """Renders the home page."""
     assert isinstance(request, HttpRequest)
@@ -70,12 +72,42 @@ def property(request):
 def new(request):
     """Renders the new page."""
     assert isinstance(request, HttpRequest)
+   
+    if request.method == 'POST': # If the form has been submitted...
+        print("Post")
+        vendor_form = VendorForm(request.POST) # A form bound to the POST data
+        purchaser_form = PurchaserForm(request.POST)
+        agent_form = AgentForm(request.POST)
+        sale_form = SaleForm(request.POST)
+
+        if vendor_form.is_valid() and purchaser_form.is_valid() and agent_form.is_valid() and sale_form.is_valid(): # All validation rules pass
+            print("Forms are valid")
+            vendor_form.save()
+            purchaser_form.save()
+            property_form.save()
+            agent_form.save()
+            sale_form.save()
+            print("Forms successfully saved to database")
+            return HttpResponseRedirect('/agents') # Redirect after POST
+        else:
+            print("Form/s not valid")
+    else:
+        print("Get")
+        vendor_form = VendorForm() # Initialise empty forms
+        purchaser_form = PurchaserForm()
+        agent_form = AgentForm()
+        sale_form = SaleForm()
+
     return render(
         request,
         'app/new.html',
         context_instance = RequestContext(request,
         {
             'title':'New Sale',
+            'vendor_form':vendor_form,
+            'purchaser_form':purchaser_form,
+            'agent_form':agent_form,
+            'sale_form':sale_form,
             'year':datetime.now().year,
         })
     )
